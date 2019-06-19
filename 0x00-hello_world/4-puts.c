a6ec31ea39fbfc6058da0ececf138e032f41de89
Yesidh <ingyagutierrez@hotmail.com>
2019-06-19 23:45:02 +0000

    script that print a string with function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_hello - prints hello with puts
+ * 
+ * 
+ * 
+ * 
+ * Return: return 0
+ */
+void print_hello(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
