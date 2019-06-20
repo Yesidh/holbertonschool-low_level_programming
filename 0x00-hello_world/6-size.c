414966386a01d63ffa53ad4f015fbee17503bae4
Yesidh <ingyagutierrez@hotmail.com>
2019-06-20 02:57:57 +0000

    remove unsigned

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,11 @@ int main(void)
 	long int d;
 	long long int e;
 	float f;
-	printf("size of a char: %ld. bytes \n", (unsigned long)sizeof(b));
-	printf("size of a int: %ld. bytes \n", (unsigned long)sizeof(c));
-	printf("size of a long int: %ld. bytes \n", (unsigned long)sizeof(d));
-	printf("size of a long long int: %ld. bytes \n", (unsigned long)sizeof(e));
-	printf("size of a float: %ld. bytes \n", (unsigned long)sizeof(f));
+
+	printf("size of a char: %ld.\n", sizeof(b));
+	printf("size of a int: %ld.\n", sizeof(c));
+	printf("size of a long int: %ld.\n", sizeof(d));
+	printf("size of a long long int: %ld.\n", sizeof(e));
+	printf("size of a float: %ld.\n", sizeof(f));
 	return (0);
 }
