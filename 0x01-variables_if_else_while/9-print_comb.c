bda37451449a96583de5508242c783629c944aff
Yesidh <ingyagutierrez@hotmail.com>
2019-06-21 00:53:32 +0000

    program that prints all posible combinations or single-digit numbers separated by , and space with the function putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print the alphabic in lowercase except q and e
+ *
+ *
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(44);
+		}
+		putchar(32);
+		a += 1;
+	}
+	putchar('\n');
+	return (0);
+}
