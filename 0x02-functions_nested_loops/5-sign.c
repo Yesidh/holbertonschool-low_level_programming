1152c495351552f191a3852b5af1da9805443058
Yesidh <ingyagutierrez@hotmail.com>
2019-06-25 16:04:41 +0000

    function that rints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: The number that check the sign
+ *
+ * Return: one if is greater than 1
+ * 0 if is zero
+ * -1 if is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		if (n == 0)
+		{
+			_putchar(0);
+			return (0);
+		}
+		else
+		{
+			_putchar('-');
+			return (-1);
+		}
+	}
+}
