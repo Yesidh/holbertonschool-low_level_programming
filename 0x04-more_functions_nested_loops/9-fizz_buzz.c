f43b7da2c99d193382ee5a902f140599a0e0cc4c
Yesidh <ingyagutierrez@hotmail.com>
2019-06-28 02:28:04 +0000

    firs commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print Fizz if is  multiples of 3
+ * Buzz if is multiplies of 5
+ * FizzBuzz if is multiplies of 5 and 3
+ * Return: nothing
+ */
+int  main(void)
+{
+	int a, b, i = 1;
+
+	while (i <= 100)
+	{
+		a = i % 3;
+		b = i % 5;
+		if ((a == 0) || (b == 0))
+		{
+			if ((a == 0) && (b != 0))
+				printf("Fizz ");
+			else if ((b == 0) && (a != 0))
+				printf("Buzz ");
+			else if ((a == 0) && (b == 0))
+				printf("FizzBuzz ");
+		}
+		else
+			printf("%d ", i);
+		i++;
+	}
+	return (0);
+}
