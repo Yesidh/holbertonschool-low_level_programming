069e91d09a0803dbc1aca8a1a11b002ba2e54bc1
Yesidh <ingyagutierrez@hotmail.com>
2019-07-02 23:21:59 +0000

    adding condition for last digit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,13 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, b;
 
-	while (str[i])
+	while(str[i])
+		i++;
+	b = i;
+	i = 0;
+	while ((str[i]) && (i <= b ))
 	{
 		_putchar(str[i]);
 		i += 2;
