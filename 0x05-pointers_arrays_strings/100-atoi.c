7dbeeeb8e27998461e6d1d49d67b216266e9870e
Yesidh <ingyagutierrez@hotmail.com>
2019-07-03 05:07:47 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _atoi - convert a string to an integer
+ *@s: string
+ *
+ *
+ * Return: nothing
+ */
+
+int _atoi(char *s)
+{
+	int a = 1, b;
+	b = a + 2;
+	s[b] = a;
+	return (0);
+}
