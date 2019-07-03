09dfdcfa4ba018262cb27f6002120cdf2aa4aaf5
Yesidh <ingyagutierrez@hotmail.com>
2019-07-03 23:30:49 +0000

    function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#include <stdio.h>
+#include <math.h>
 /**
  * _atoi - convert a string to an integer
  *@s: string
@@ -10,8 +10,25 @@
 
 int _atoi(char *s)
 {
-	int a = 1, b;
-	b = a + 2;
-	s[b] = a;
-	return (0);
+	int i = 0, m = 0, j = 0;
+	float sum = 0, num = 0;
+
+	while ((s[i] == '-') && ((s[i + 1] >= 0) || (s[i] <= 9)))
+	{
+		m++;
+		if ((s[i] >= 0) || (s[i] <= 9))
+		{
+			if (num == 0)
+				j = i;
+			num++;
+		}
+		i++;
+	}
+	while (((s[j] >= 0) || (s[j] <= 9)) && num > 0)
+	{
+		sum = sum + s[j] * pow(10, (num - 1));
+		j++;
+		num--;
+	}
+return (sum);
 }
