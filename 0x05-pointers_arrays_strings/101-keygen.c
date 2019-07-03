6c9b6a5e7de9cdcc3fa88c9ac4d71565d098ac8e
Yesidh <ingyagutierrez@hotmail.com>
2019-07-03 05:14:29 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - convert a string to an integer
+ *
+ *
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	int a = 1, b;
+	b = a + 2;
+	printf("%d", b);
+
+	return (0);
+}
