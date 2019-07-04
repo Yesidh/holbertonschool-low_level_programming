216cc210c627a622f66dc140302a04d7a56bf9f4
Yesidh <ingyagutierrez@hotmail.com>
2019-07-04 22:04:40 +0000

    changing all my login with a logic with ascci character

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,25 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0, k = 0;
+	int i = 0, j = 0, r = 0;
 
-	while ((s1[i] == s2[j]) && s1[i])
+	while (s1[i] && r == 0)
 	{
+		r = s1[i] - s2[j];
 		i++;
 		j++;
 	}
-	if (s1[i] == '\0')
-		return (k);
-	i = 0;
-	j = 0;
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	if (i > j)
-		return (1);
-	if (j > i)
-		return (-1);
-
-	return (0);
+	return (r);
 }
