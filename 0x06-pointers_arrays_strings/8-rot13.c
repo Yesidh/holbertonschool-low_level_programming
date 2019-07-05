eaccfb377709accdd83002447007569bfccff9dc
Yesidh <ingyagutierrez@hotmail.com>
2019-07-05 04:19:46 +0000

    function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string
+ *
+ *
+ * Return: string s
+ */
+char *rot13(char *s)
+{
+	int i = 0, j = 0;
+
+	s[i] = s[i] + j;
+	return (s);
+}
