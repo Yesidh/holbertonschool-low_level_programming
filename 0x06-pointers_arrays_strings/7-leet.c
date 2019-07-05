10b1ef73c5c68a1be95231bbb55c35803b753d19
Yesidh <ingyagutierrez@hotmail.com>
2019-07-05 04:13:39 +0000

    function that encodes a string into 1373

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string into 1337
+ * @s: string
+ *
+ *
+ * Return: string s
+ */
+char *leet(char *s)
+{
+	int i = 0, j = 0;
+
+	s[i] = s[i] + j;
+	return (s);
+}
