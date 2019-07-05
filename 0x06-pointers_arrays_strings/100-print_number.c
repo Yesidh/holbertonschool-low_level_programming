55af368cd54c91ea214d4a53050965a87b855aa2
Yesidh <ingyagutierrez@hotmail.com>
2019-07-05 04:36:44 +0000

    function that prints an integer

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_number - encodes a string using rot13
+ * @n: string
+ *
+ *
+ * Return: string s
+ */
+void print_number(int n)
+{
+	int i = 0, j = 0;
+
+	n = i + j;
+	n++;
+}
