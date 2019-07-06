850393b971643a95b91b177477468a82c518bb99
Yesidh <tim@timrich.org>
2019-07-06 00:05:51 -0500

    perrada time

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_buffer - prints a buffer
+ * @b: pointer
+ * @size: bytes of the buffer pointed
+ *
+ *
+ * Return: pointer with the result
+ */
+void print_buffer(char *b, int size)
+{
+	int i = 0, j = 0;
+
+	size--;
+	b[i] = i + j;
+}
