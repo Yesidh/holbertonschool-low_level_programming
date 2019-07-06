89a40a92a3cdeec3d5ee57b2302194801e9fe88e
Yesidh <tim@timrich.org>
2019-07-05 23:57:58 -0500

    perrada time

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * infinite_add - adds two numbers
+ * @n1: number 1
+ * @n2: number 2
+ * @r: buffer
+ * @size_r: buffer size
+ * Return: pointer with the result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0, j = 0;
+
+	size_r--;
+	n1[i] = i + j;
+	n2[j] = 0;
+	r[i] = 23;
+	return (r);
+}
