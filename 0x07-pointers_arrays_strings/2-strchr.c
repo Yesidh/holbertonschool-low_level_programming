b43f5bc960c7c8529ebc5db3627ba99bf2b8ab89
Yesidh <tim@timrich.org>
2019-07-08 15:02:18 -0500

    function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strchr - locates a character in a string
+ *@s: string to search
+ *@c:character to be find
+ *
+ *
+ * Return: string after the first similarity
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return ('\0');
+}
