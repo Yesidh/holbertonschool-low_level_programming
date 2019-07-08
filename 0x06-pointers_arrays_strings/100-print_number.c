c1365ce1f8b4e51fec6ce52433443506f66f903d
Yesidh <tim@timrich.org>
2019-07-08 00:04:15 -0500

    adding logic to print number in the second time cicle

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -8,19 +8,27 @@
  */
 void print_number(int n)
 {
-	int n2 = 0;
+	int n2 = 0, cont = 0;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * (-1);
 	}
-	while (n >= 0)
+	while (cont < 2)
 	{
-		n2 = (n2 % 10) * 10 + (n / 10);
-		_putchar((n % 10) + '0');
-		n = n / 10;
-		if (n == 0)
-			n = -1;
+		while (n >= 10)
+		{
+			n2 = n2 * 10 + (n % 10);
+			if (cont == 1)
+				_putchar((n % 10) + '0');
+			n = n / 10;
+		}
+		if (cont == 1)
+			_putchar(n + '0');
+		n2 = n2 * 10 + n;
+		cont++;
+		n = n2;
+		n2 = 0;
 	}
 }
