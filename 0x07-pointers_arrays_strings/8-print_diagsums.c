62dfb39b2fc74e38e348a4b38c165b4ec782d67e
Yesidh <tim@timrich.org>
2019-07-08 22:11:31 -0500

    betty style

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,14 +10,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i = 0, sum = 0, sum2 = 0, i2 = size -1;
+	int i = 0, sum = 0, sum2 = 0, i2 = size - 1;
 
 	while (i <= ((size * size) - 1))
 	{
 		sum = sum + a[i];
 		i = i + (size + 1);
 		sum2 = sum2 + a[i2];
-		i2 = (size -1) + i2;
+		i2 = (size - 1) + i2;
 	}
 	printf("%d, %d\n", sum, sum2);
 
