65cd53b291b1ea21c18c063bdc443c68f37ce6d9
Yesidh <tim@timrich.org>
2019-07-09 20:26:28 -0500

    count the bytes in the string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i = 0, j = 0;
 
-	while (s[i])
+	while (s[j])
+		j++;
+	j++;
+	while (i <= j)
 	{
 		if (s[i] == c)
 			return (s + i);
 		i++;
 	}
-	if (s[i] == '\0')
-		return ('\0');
 	return ('\0');
 }
