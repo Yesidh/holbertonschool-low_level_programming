696631e9ef9464fd29dd05f488962c327f2329e6
Yesidh <tim@timrich.org>
2019-07-10 23:26:39 -0500

    perrada time

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * is_palindrome - find if a string is palindrome
+ *@s: string for check
+ *
+ *
+ * Return: one is is palindrome 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	int i = 1;
+
+	if (*s == i)
+		return (0);
+	return (1);
+}
