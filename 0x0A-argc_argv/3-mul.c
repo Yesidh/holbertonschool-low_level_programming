a9c878bf3d67695a1280a60a21f683eeae6be4a5
Yesidh <tim@timrich.org>
2019-07-14 22:22:46 -0500

    betty style

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,8 +10,8 @@ int main(int argc, char *argv[])
 {
 	if (argc <= 2)
 	{
-			printf("Error\n");
-			return (0);
+		printf("Error\n");
+		return (0);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (1);
