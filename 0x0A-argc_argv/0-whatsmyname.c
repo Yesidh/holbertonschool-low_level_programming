ada27c4e1088c5a95b2f112e467c9d90c04acae6
Yesidh <tim@timrich.org>
2019-07-14 20:43:29 -0500

    betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - print first argument
- *
- *
+ *@argc: size of argv array
+ *@argv: array with size argc
  * Return: the first argument in the program
  */
 int main(int argc, char *argv[])
