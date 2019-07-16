d671ea2bfa99a5934868eae3c4db1729da537f4a
Yesidh <tim@timrich.org>
2019-07-15 23:09:42 -0500

    adding all logical for coins of 25 10 5 2 1

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,19 +8,41 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = 1;
+	int num, change = 0;
 
 	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argv[i]) < 0)
+	if (atoi(argv[1]) < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	printf("listos pa programar\n");
-	printf("%d\n", argc);
+	num = atoi(argv[1]);
+	if (num >= 25)
+	{
+		change += num / 25;
+		num = num % 25;
+	}
+	if (num >= 10)
+	{
+		change += num / 10;
+		num = num % 10;
+	}
+	if (num >= 5)
+	{
+		change += num / 5;
+		num = num % 5;
+	}
+	if (num >= 2)
+	{
+		change += num / 2;
+		num = num % 2;
+	}
+	if (num >= 1)
+		change += num;
+	printf("%d\n", change);
 	return (0);
 }
