c5d65cafa5f28dc35313dbcfabf4a90faf721ff1
Yesidh <tim@timrich.org>
2019-07-16 14:29:57 -0500

    returns a pointer to a newly allocated space in memory, wich contains a copy of the string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer wich contain a copy of string
+ *@str: string to be copied
+ *
+ *
+ * Return: NULL if string and memory are empy, a pointer with a copy of string
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *str2;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+	}
+	str2 = malloc(i * sizeof(char));
+	if (str2 == NULL)
+		return (NULL);
+	for ( ; i >= 0; i--)
+		str2[i] = str[i];
+	return (str2);
+
+}
