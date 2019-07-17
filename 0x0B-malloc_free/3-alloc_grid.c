d9498abe159a5a6952df179c3c1116d6e4990504
Yesidh <tim@timrich.org>
2019-07-16 20:04:13 -0500

    changging return array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,19 +9,19 @@
  */
 int **alloc_grid(int width, int height)
 {
-	unsigned int *array;
+	unsigned int **array;
 
 	unsigned int i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	array = malloc(((width + height) * sizeof(unsigned int)) + 1);
-	if (array == null)
-		retrun (NULL);
+	if (array == NULL)
+		return (NULL);
 	for (i = 0; i <= width; i++)
 	{
 		for (j = 0; j <= height; j++)
 			array[i][j] = 0;
 	}
-	return (array);
+	return (0);
 }
