b8d4e03f3f13e43c1f09b19bf9c2af1c03dccaeb
Yesidh <tim@timrich.org>
2019-07-22 10:47:37 -0500

    define a new type struct dog whit three elementes/members

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog
+#define dog
+/**
+ * struct dog - basic way to create a structure
+ * @name: firs member, name of the dog
+ * @age: Second member, age of the dod
+ * @owner: Third member, owner of the dog
+ *
+ * Description: how to create a basic structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
