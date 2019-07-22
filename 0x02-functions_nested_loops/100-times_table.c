d65f467bbce8dfd74e4e77ced405aba29b3c6c6a
Yesidh <tim@timrich.org>
2019-07-22 00:26:31 -0500

    changing the logical

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,59 @@
 #include "holberton.h"
 /**
- * times_table -print the tables starting with 0 ending in 9
- *
+ * print_times_table -print the tables starting with 0 ending in 9
+ * @n: times to print a table
  *
  * Return: nothing
  */
 void print_times_table(int n)
 {
-  int i = 0, j = 0, mul,sd;
+	int i = 0, j = 0, mul;
 
-  if((n >= 0) && (n <= 15))
-    { 
-      while (i <= n)
+	if ((n >= 0) && (n <= 15))
 	{
-	  while (j <= n)
-	    {
-	      mul = i * j;
-	      _putchar((mul / 100) + '0');
-	      sd = mul % 100;
-	      if (sd > 9)
+		while (i <= n)
 		{
-		  _putchar(' ');
-		  _putchar((sd / 10) + '0');
-		  _putchar((sd % 10) + '0');
+			while (j <= n)
+			{
+				mul = i * j;
+				if (mul == 0 && j > 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(mul + '0');
+				}
+				if (mul == 0 && j == 0)
+					_putchar(mul + '0');
+				if (mul > 9 && mul < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((mul / 10) + '0');
+					_putchar((mul % 10) + '0');
+				}
+				if (mul > 0 && mul < 10 && i > 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(mul + '0');
+				}
+				if (mul >= 100)
+				{
+					_putchar(' ');
+					_putchar((mul / 100) + '0');
+					mul = mul % 100;
+					_putchar((mul / 10) + '0');
+					_putchar((mul % 10) + '0');
+				}
+				if (j < n)
+					_putchar(',');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+			j = 0;
 		}
-	      else
-		{
-		  if (j >= 1)
-		    {
-		      _putchar(' ');
-		      _putchar(' ');
-		      _putchar(sd + '0');
-		    }
-		  else
-		    _putchar(sd + '0');
-		}
-	      if (j < n)
-		_putchar(',');
-	      j++;
-	    }
-	  _putchar('\n');
-	  i++;
-	  j = 0;
 	}
-    }
 }
