ef9f512e7d83b04bec0408274fa801040aa9891c
Yesidh <tim@timrich.org>
2019-07-22 23:19:44 -0500

    nothing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef gerente
 #define gerente
-void free_dog(dog_t *d);
 /**
  * struct dog - basic way to create a structure
  * @name: firs member, name of the dog
@@ -18,4 +17,5 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
