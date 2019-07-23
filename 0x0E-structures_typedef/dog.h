7e3e9d5bd41eb2d922ca86474ed43063f04169be
Yesidh <tim@timrich.org>
2019-07-22 22:09:37 -0500

    adding typedef in the structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,12 @@
  *
  * Description: how to create a basic structure
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-struct dog *dog_t;
 #endif
