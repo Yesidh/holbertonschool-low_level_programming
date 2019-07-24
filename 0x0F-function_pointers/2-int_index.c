f96438dcdbead252df496161f405c6a61132e905
Yesidh <tim@timrich.org>
2019-07-24 16:04:50 -0500

    adding if array and cmp no NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,14 +11,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if ((*cmp)(array[i]))
-			return (i);
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			if ((*cmp)(array[i]))
+				return (i);
+		}
+		if (i == size)
+			return (-1);
 	}
-	if (i == size)
-		return (-1);
 	return (-1);
 }
