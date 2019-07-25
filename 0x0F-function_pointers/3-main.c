c752d5b69ff4a3b3def494b5093847bd17df4fce
Yesidh <tim@timrich.org>
2019-07-24 23:33:10 -0500

    program that print mul, sum, div, sub and mod

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main - function to print basic operations like a calculator, add, div, sub
+ * mod and mul
+ * @argc: size of array
+ * @argv: array with size arguments
+ *
+ * Return: the operation between two integers
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, oper;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
+	    argv[2][0] != '%' && argv[2][0] != '/')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	oper = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", oper);
+	return (0);
+
+}
