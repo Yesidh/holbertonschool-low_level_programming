3adbd259724600d7317a048a0aa4aa56b11965b9
Yesidh <tim@timrich.org>
2019-07-27 19:35:15 -0500

    adding the prototype void print_all(const char * const format, ...)

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,10 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+typedef struct fs
+{
+	char *c;
+	void (*ptr)();
+} fs_1;
 #endif
