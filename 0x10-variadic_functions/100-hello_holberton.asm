106ef69f0e867093382d91b8ffa819979cb05928
Yesidh <tim@timrich.org>
2019-07-29 00:59:08 -0500

    adding descriptions

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,16 @@
 section .data
-	text db "Hello, Holberton",10
+	text db "Hello, Holberton",10 	;defines bytes and labels for memory address "text"
 
 section .text
 	global _start
 
-_start:
-	mov rax, 1
+_start:				;this line es called label
+	mov rax, 1		;this block is for sys_write(1,text,17)
 	mov rdi, 1
 	mov rsi, text
 	mov rdx, 17
 	syscall
 
-	mov rax, 60
+	mov rax, 60 		;in this line start the sys_exit(0)
 	mov rdi, 0
 	syscall
