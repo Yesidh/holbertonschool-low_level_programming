09ee49fdd1461476634f03eda18a5fe2b03ba85e
Yesidh <tim@timrich.org>
2019-07-29 01:30:45 -0500

    changing 10 by 0xa in the data section line and deleting spaces afeter invoked rax, rdi, rsi, rdx

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,17 +1,17 @@
 section .data
-	msg db "Hello, Holberton", 10 	;defines bytes and labels for memory address "text"
+	msg db "Hello, Holberton",0xa 	;defines bytes and labels for memory address "text"
 	msglen equ $ - msg
 
 section .text
 	global _start
 
 _start:				;this line es called label
-	mov rax, 1		;this block is for sys_write(1,text,17)
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, msglen
+	mov rax,1		;this block is for sys_write(1,text,17)
+	mov rdi,1
+	mov rsi,msg
+	mov rdx,msglen
 	syscall
 
-	mov rax, 60 		;in this line start the sys_exit(0)
-	mov rdi, 0
+	mov rax,60 		;in this line start the sys_exit(0)
+	mov rdi,0
 	syscall
