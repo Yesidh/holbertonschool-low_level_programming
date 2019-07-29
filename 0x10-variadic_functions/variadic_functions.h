d9e051eada4e9c8e0ecd1426643d2d0a4d8089b7
Yesidh <tim@timrich.org>
2019-07-29 00:58:09 -0500

    adding the struct description

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,6 +7,12 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct fs - struc for call functions
+ * @c: pointer char
+ * @ptr: pointer function
+ * Description: struc to use an array of functions
+ */
 typedef struct fs
 {
 	char *c;
