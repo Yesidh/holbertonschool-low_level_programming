a27f7d682a7e7a1ad47f088c2d2b44e2c2578d61
Yesidh <tim@timrich.org>
2019-07-29 01:22:16 -0500

    adding the line msglen in the data section and change the value 17 for msglen in the write section

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,5 +1,6 @@
 section .data
-	msg db "Hello, Holberton",10 	;defines bytes and labels for memory address "text"
+	msg db "Hello, Holberton", 10 	;defines bytes and labels for memory address "text"
+	msglen equ $ - msg
 
 section .text
 	global _start
@@ -8,7 +9,7 @@ _start:				;this line es called label
 	mov rax, 1		;this block is for sys_write(1,text,17)
 	mov rdi, 1
 	mov rsi, msg
-	mov rdx, 17
+	mov rdx, msglen
 	syscall
 
 	mov rax, 60 		;in this line start the sys_exit(0)
