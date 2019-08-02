f7ac16042ac7ebb32377055ff0be9724daac7b54
Yesidh <944@holbertonschool.com>
2019-08-01 20:01:38 -0500

    Betty Style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+/**
+ * first - function for print a string before the main
+ * Return: nothing
+ */
+
 void  __attribute__((constructor))first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
