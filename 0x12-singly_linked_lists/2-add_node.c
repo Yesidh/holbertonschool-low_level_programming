dfa430a7e43b5ef7810a9c7c058ca0732402a324
Yesidh <944@holbertonschool.com>
2019-08-01 23:39:57 -0500

    #Perrada time

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *add_node - function that add a new node at the beggining of a list_t list
+ *@head: head of the list
+ *@str: pointer to a char
+ *Return: nothing
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (head && str && new)
+		printf("error\n");
+	new->str = "yesid";
+	new->len = 35;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
