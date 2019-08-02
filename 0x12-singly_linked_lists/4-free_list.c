a74744492c157447c6af767358d3175ac3648b64
Yesidh <944@holbertonschool.com>
2019-08-01 23:48:27 -0500

    #Only betty

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ *free_list - function that frees a list_t list
+ *@head: head of the list
+ *Return: nothing
+ */
+void free_list(list_t *head)
+{
+	if (head)
+		printf("error");
+
+}
