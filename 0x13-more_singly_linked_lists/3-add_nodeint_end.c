5ad11583b63f03086fbe3a6b4590420ec1e96a19
Yesidh <944@holbertonschool.com>
2019-08-06 16:27:08 -0500

    #Fix: adding the condition when the pointer &*head is null

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *newnode, *recorrer;
 
-	if (head == NULL && n)
+	if (&*head)
 		printf("Error\n");
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
