4fca7508929af04f194e9e873110b53efdc5ac9f
Yesidh <944@holbertonschool.com>
2019-08-06 16:31:27 -0500

    #Fix: changing the condition when the &*head is NULL

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *newnode, *recorrer;
 
 	if (&*head)
-		printf("Error\n");
+		return (NULL);
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
