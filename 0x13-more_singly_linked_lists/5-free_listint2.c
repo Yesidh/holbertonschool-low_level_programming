56dac288ee9377be6aeb9e430a15eb44a9b23ed8
Yesidh <944@holbertonschool.com>
2019-08-06 14:15:03 -0500

    #Fix: adding the condition when the head is null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stddef.h>
 /**
  * free_listint2 - Function that frees listint_t list
  * @head: pointer to pointer  to point to head
@@ -9,6 +8,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *paux;
 
+	if (&*head == NULL)
+		return;
 	paux = *head;
 	while (*head)
 	{
