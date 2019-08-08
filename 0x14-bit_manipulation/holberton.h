2356d2354f1888be19ccfb76b624c6146b66c642
Yesidh <944@holbertonschool.com>
2019-08-08 17:34:24 -0500

    #Fix: adding the _putchar prototype

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,7 @@
 #ifndef BITS
 #define BITS
 #include <stdio.h>
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned int n, unsigned int index);
