4e04f4e39a7d616c73116dddc273780eec52ceb3
Yesidh <944@holbertonschool.com>
2019-08-13 18:20:30 -0500

    #Fix: adding the condition when text is NULL

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,6 +15,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+	{
+		if (fd == 2)
+			return (-1);
+	}
 	ch1 = strlen(text_content);
 	ch2 = write(fd, text_content, ch1);
 	if (ch2 == -1)
