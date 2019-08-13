aff635d286ced13b29603059dce5b8e7584ad734
Yesidh <944@holbertonschool.com>
2019-08-13 15:36:21 -0500

    #Fix: changing #include <fontl.h> by <fcntl.h>

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fontl.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
