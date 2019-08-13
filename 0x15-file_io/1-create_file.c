61f5d3eaddd3b359f63f73623b840a3d4fb99c34
Yesidh <944@holbertonschool.com>
2019-08-13 17:12:03 -0500

    #Feature: Function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file
+ * @text_content: content to write in the file
+ * Return: 1 on success -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t ch1;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	ch1 = strlen(text_content);
+	write(fd, text_content, ch1);
+	if (ch1 == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
