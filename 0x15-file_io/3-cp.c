545094c93ade27db176b7b5e8bf73784066a0a83
Yesidh <944@holbertonschool.com>
2019-08-13 23:53:55 -0500

    #Feature: program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+/**
+ * main - function that appends text at the end of a file
+ * @argc: numbers of arguments
+ * @a: array with arguments
+ * Return: 1 on success -1 on failure
+ */
+int main(int argc, char **a[])
+{
+	ssize_t  fd1, fd2, sbuf, sw;
+
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprint(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd2 = open(a[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == 2)
+	{
+		dprint(STDERR_FILENO, "Error: Can't write to %s\n", a[2]);
+		exit(99);
+	}
+	fd1 = open(a[1], O_RDONLY);
+	if (fd1 == 2)
+	{
+		dprint(STDERR_FILENO, "Error: Can't read from file %s\n", a[1]);
+		exit(98);
+	}
+	while (sbuf == 1024)
+	{
+		sbuf = read(fd, buf, 1024);
+		sw = write(fd1, buf, sbuf);
+	}
+	if (close(fd1) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: cant't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) < 0)
+	{
+		dprint(STDERR_FILENO, "Error: Cant't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
+}
