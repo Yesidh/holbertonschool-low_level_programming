2ccce531efdeec1fc1ca46cdc4d36ace00fcd3d7
Yesidh <944@holbertonschool.com>
2019-08-14 20:03:59 -0500

    #Fix: changing parameters in the write function and making a nuw function to close de fd1 and fd2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,24 @@
 #include "holberton.h"
 /**
+ * close_fd - function that close two files using file descriptors
+ * @fd1: file descriptor 1
+ * @fd2: file descriptor 2
+ * Return: nothing
+ */
+void close_fd(int fd1, int fd2)
+{
+	if (close(fd1) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: cant't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Cant't close fd %d\n", fd2);
+		exit(100);
+	}
+}
+/**
  * main - function that appends text at the end of a file
  * @argc: numbers of arguments
  * @a: array with arguments
@@ -8,7 +27,6 @@
 int main(int argc, char **a)
 {
 	int  fd1, fd2, sbuf, sw;
-
 	char buf[1024];
 
 	if (argc != 3)
@@ -16,7 +34,6 @@ int main(int argc, char **a)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd2 = open(a[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == 2)
 	{
@@ -29,22 +46,21 @@ int main(int argc, char **a)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", a[1]);
 		exit(98);
 	}
+	sbuf = 1024;
 	while (sbuf == 1024)
 	{
 		sbuf = read(fd1, buf, 1024);
+		if (sbuf == 2)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", a[1]);
+			exit(98);
+		}
 		sw = write(fd2, buf, sbuf);
-		if (sw == -1)
-			return (-1);
-	}
-	if (close(fd1) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: cant't close fd %d\n", fd1);
-		exit(100);
-	}
-	if (close(fd2) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Cant't close fd %d\n", fd2);
-		exit(100);
+		if (sw == 2)
+		{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", a[2]);
+		exit(99);
+		}
 	}
 	return (0);
 }
