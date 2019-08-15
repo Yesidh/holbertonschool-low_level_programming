ae09e34f0c58db55167e39c1f415bd3529ac3132
Yesidh <944@holbertonschool.com>
2019-08-14 20:30:57 -0500

    #Fix: changing **a for *argv[]

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ void close_fd(int fd1, int fd2)
  * @a: array with arguments
  * Return: 1 on success -1 on failure
  */
-int main(int argc, char **a)
+int main(int argc, char *argv[])
 {
 	int  fd1, fd2, sbuf, sw;
 	char buf[1024];
@@ -34,16 +34,16 @@ int main(int argc, char **a)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd2 = open(a[2], O_RDWR | O_CREAT | O_TRUNC | O_APPEND, 0664);
+	fd2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", a[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	fd1 = open(a[1], O_RDONLY);
+	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", a[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	sbuf = 1024;
@@ -52,13 +52,13 @@ int main(int argc, char **a)
 		sbuf = read(fd1, buf, 1024);
 		if (sbuf == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", a[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		sw = write(fd2, buf, sbuf);
 		if (sw == -1)
 		{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", a[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 		}
 	}
