4b8b0524d88ff080a76401969e92ab25aec97457
Yesidh <944@holbertonschool.com>
2019-09-15 18:36:56 -0500

    #Fix: deleting print into if statement

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -11,10 +11,7 @@ size_t dlistint_len(const dlistint_t *h)
 	if (h == NULL)
 		return (0);
 	if (h->next == NULL && h->prev == NULL)
-	{
-		printf("%d\n", h->n);
 		return (1);
-	}
 	for (i = 0; h; i++)
 		h = h->next;
 	return (i);
