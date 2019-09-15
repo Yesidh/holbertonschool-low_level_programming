860637f647b17c5e04d198a7b732a90f6c8b9ac2
Yesidh <944@holbertonschool.com>
2019-09-15 17:37:41 -0500

    #Fix: betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,12 +13,12 @@ size_t print_dlistint(const dlistint_t *h)
 	if (h->next == NULL && h->prev == NULL)
 	{
 		printf("%d\n", h->n);
-		return(1);
+		return (1);
 	}
 	for (i = 0; h; i++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return(i);
+	return (i);
 }
