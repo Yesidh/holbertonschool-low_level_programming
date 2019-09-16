f283c7136f52be5c33767cfc4d72ef480c261295
Yesidh <944@holbertonschool.com>
2019-09-16 00:15:50 -0500

    #Fix: chanign puax2 by paux2 in the else condition

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -31,7 +31,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		paux1 = (paux2->next)->next;
 		free(paux2->next);
-		puax2->next = paux1;
+		paux2->next = paux1;
 		return (1);
 	}
 }
