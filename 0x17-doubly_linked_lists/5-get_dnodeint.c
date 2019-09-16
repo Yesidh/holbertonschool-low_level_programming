f17eac3e63a579157883416cbdb358cc36529ddd
Yesidh <944@holbertonschool.com>
2019-09-15 21:19:58 -0500

    #Feature: Function that return the node from a double linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Function that return a node
+ *                         into a double linked list
+ * @head: pointer where start the list
+ * @index: position for return node
+ * Return: the node in the index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	size_t i;
+
+	if (head == NULL)
+		return (0);
+	for (i = 0; i < index; i++)
+		head = head->next;
+	return (head);
+}
