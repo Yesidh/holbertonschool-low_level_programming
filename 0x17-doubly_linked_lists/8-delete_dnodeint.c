1cb13332bfa928fbe21310b3f0d3cd5ac46c6503
Yesidh <944@holbertonschool.com>
2019-09-15 23:26:40 -0500

    #Fix: changing idx by index in the function

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,10 +2,10 @@
 /**
  * delete_dnodeint_at_index - Function that delete a node at a given pos
  * @head: double pointer where start the list
- * @idx: position for delete the node
+ * @index: position for delete the node
  * Return: 1 if it succeeded, -1 if it failde
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int idx)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i;
 	dlistint_t *paux1, *paux2;
@@ -13,14 +13,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int idx)
 	if (*head == NULL)
 		return (-1);
 	paux2 = *head;
-	if (idx == 0)
+	if (index == 0)
 	{
 		paux1 = paux2->next;
 		free(*head);
 		*head = paux1;
 		return (1);
 	}
-	for (i = 0; i < (idx - 1); i++)
+	for (i = 0; i < (index - 1); i++)
 	{
 		if (paux1)
 			paux1 = paux1->next;
