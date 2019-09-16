76e6827e326f3108404c088ceca47320d87770a5
Yesidh <944@holbertonschool.com>
2019-09-15 21:22:09 -0500

    #Fix: changin return (0) for return (NULL) in the first If

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	size_t i;
 
 	if (head == NULL)
-		return (0);
+		return (NULL);
 	for (i = 0; i < index; i++)
 		head = head->next;
 	return (head);
