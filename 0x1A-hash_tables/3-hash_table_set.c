ad8a695b81d5faf80c1d6bc5e23980d6bb088cc7
Yesidh <944@holbertonschool.com>
2019-10-11 15:09:54 -0500

    #Fix: adding condition when key and value are empty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL)
 		return (0);
 
-	if (key == NULL)
+	if (key == "" || value == "")
 		return (0);
 
 	node = malloc(sizeof(hash_node_t));
