ed3c6f78a2ef16223df63756c1b44f0f7996c768
Yesidh <944@holbertonschool.com>
2019-10-11 16:42:00 -0500

    #Fix: adding logical for update a key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,32 +10,43 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *node = NULL;
-
+	hash_node_t *node = NULL, *aux = NULL;
 
 	if (ht == NULL)
 		return (0);
-
 	if (strcmp(key,  "") == 0 || strcmp(value, "") == 0)
 		return (0);
-
+	index = key_index((unsigned char *) key, ht->size);
+	aux = ht->array[index];
+	if (ht->array[index] != NULL)
+	{
+		if (strcmp(key, ht->array[index]->key) == 0)
+			ht->array[index]->value = strdup(value);
+		else
+		{
+			while (aux->next != NULL)
+			{
+				if (strcmp(key, aux->key) == 0)
+					ht->array[index]->value = strdup(value);
+				aux = aux->next;
+			}
+		}
+		return (1);
+	}
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
-
-	index = key_index((unsigned char *) key, ht->size);
 	node->key = strdup(key);
 	node->value = strdup(value);
-		if (ht->array[index] == NULL)
-		{
-			node->next = NULL;
-			ht->array[index] = node;
-		}
-		else
-		{
-			node->next = ht->array[index];
-			ht->array[index] = node;
-		}
-
-		return (1);
+	if (ht->array[index] == NULL)
+	{
+		node->next = NULL;
+		ht->array[index] = node;
+	}
+	else
+	{
+		node->next = ht->array[index];
+		ht->array[index] = node;
+	}
+	return (1);
 }
