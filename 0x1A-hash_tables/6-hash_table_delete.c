4d8a5991dff60ea48719b49049b8b8946f03e6b7
Yesidh <944@holbertonschool.com>
2019-10-11 21:53:29 -0500

    #Fix: betty style

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /**
  * hash_table_delete - function that deletes a hash table
- * @ht
+ * @ht: hast table for delete
  * Return: Nothing
  */
 void hash_table_delete(hash_table_t *ht)
