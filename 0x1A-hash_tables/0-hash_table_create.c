c25377969c9e432815f6684d35dc0e65a7531285
Yesidh <944@holbertonschool.com>
2019-10-11 22:29:38 -0500

    #Fix: filling with 0 values

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,6 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ptr->array = malloc(size * sizeof(hash_node_t *));
 	if (ptr->array == NULL)
 		return (NULL);
+	memset(ptr->array, 0, size * sizeof(hash_node_t *));
 
 	ptr->size = size;
 	return (ptr);
