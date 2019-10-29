cb409eafc947b2a720a69989cdc6a7ec1d451b22
Yesidh <944@holbertonschool.com>
2019-10-28 20:15:20 -0500

    #Fix: adjustion initial conditions about list and *list

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -12,38 +12,38 @@ void insertion_sort_list(listint_t **list)
 	listint_t *aux1, *aux2, *aux3;
 	int flag = 0, flag2 = 0;
 
-	if (*list == NULL || list == NULL || (*list)->next == NULL)
-		return;
-
-	aux1 = *list;
-	aux1 = aux1->next;
-	while (aux1)
+	if (list && (*list) && (*list)->next)
 	{
-		aux3 = aux1->next;
-		aux2 = aux1;
-		while (aux1->prev != NULL && aux1->n < aux1->prev->n)
+		aux1 = *list;
+		aux1 = aux1->next;
+		while (aux1)
 		{
-			aux2 = aux1->prev;
-			aux1->prev = aux2->prev;
-			aux2->next = aux1->next;
-			if (aux2->prev != NULL)
+			aux3 = aux1->next;
+			aux2 = aux1;
+			while (aux1->prev != NULL && aux1->n < aux1->prev->n)
 			{
-				aux2->prev->next = aux1;
-				flag = 1;
+				aux2 = aux1->prev;
+				aux1->prev = aux2->prev;
+				aux2->next = aux1->next;
+				if (aux2->prev != NULL)
+				{
+					aux2->prev->next = aux1;
+					flag = 1;
+				}
+				if (aux1->next != NULL)
+					aux1->next->prev = aux2;
+				aux1->next = aux2;
+				aux2->prev = aux1;
+				if (flag == 0)
+					*list = aux1;
+				print_list(*list);
+				flag = 0;
+				flag2 = 1;
 			}
-			if (aux1->next != NULL)
-				aux1->next->prev = aux2;
-			aux1->next = aux2;
-			aux2->prev = aux1;
-			if (flag == 0)
-				*list = aux1;
-			print_list(*list);
-			flag = 0;
-			flag2 = 1;
+			if (flag2 == 0)
+				aux1 = aux1->next;
+			else
+				aux1 = aux3;
 		}
-		if (flag2 == 0)
-			aux1 = aux1->next;
-		else
-			aux1 = aux3;
 	}
 }
