80c16d76c43fad13da8bccfc412bfcc4c9fc551f
Yesidh <944@holbertonschool.com>
2019-11-16 00:57:35 -0500

    [Fix]:changin conditions for laterals

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -21,10 +21,10 @@ def island_perimeter(grid):
                 if grid[x][y]:
                     if x == 0 or grid[x - 1][y] == 0:
                         perimeter += 1
-                    if x == (len(grid[x]) - 1) or grid[x + 1][y] == 0:
+                    if x == (len(grid) - 1) or grid[x + 1][y] == 0:
                         perimeter += 1
                     if y == 0 or grid[x][y - 1] == 0:
                         perimeter += 1
-                    if y == len(grid) or grid[x][y + 1] == 0:
+                    if y == len(grid[x]) or grid[x][y + 1] == 0:
                         perimeter += 1
     return perimeter
