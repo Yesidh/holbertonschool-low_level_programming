1bc1cec11b05f3945808bc3afc3674bbc0e78bfd
Yesidh <944@holbertonschool.com>
2019-11-16 00:53:58 -0500

    [Fix]: changing and by or

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -19,12 +19,12 @@ def island_perimeter(grid):
     for x in range(len(grid)):
         for y in range(len(grid[x])):
                 if grid[x][y]:
-                    if x > 0 and grid[x - 1][y] == 0:
+                    if x == 0 or grid[x - 1][y] == 0:
                         perimeter += 1
-                    if x < (len(grid[x]) - 1) and grid[x + 1][y] == 0:
+                    if x == (len(grid[x]) - 1) or grid[x + 1][y] == 0:
                         perimeter += 1
-                    if y > 0 and grid[x][y - 1] == 0:
+                    if y == 0 or grid[x][y - 1] == 0:
                         perimeter += 1
-                    if y < len(grid) and grid[x][y + 1] == 0:
+                    if y == len(grid) or grid[x][y + 1] == 0:
                         perimeter += 1
     return perimeter
