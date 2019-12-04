aa00b9aa72f49de5a84cc20f31e4c971f1297529
Yesidh <944@holbertonschool.com>
2019-12-04 14:52:12 -0500

    [Fix]:changing the logical for the counter variable and add a return o

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -10,21 +10,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t counter = 0;
+	size_t counter = 0, counter1 = 0;
 
 	if (tree)
 	{
 
 		if (tree->left)
-		{
-			counter++;
-			binary_tree_height(tree->left);
-		}
+			counter = binary_tree_height(tree->left);
 		if (tree->right)
-		{
-			counter++;
 			binary_tree_height(tree->right);
-		}
+		else
+			return (0);
 	}
-	return (counter);
+	return (1 + counter + counter1);
 }
