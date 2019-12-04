d57d36adc5549c82edf040e42c0ccd39a310752e
Yesidh <944@holbertonschool.com>
2019-12-04 15:12:25 -0500

    [Fix]:deleting counter1

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -10,15 +10,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t counter = 0, counter1 = 0;
+	size_t counter = 0;
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 	if (tree->left)
 		counter = binary_tree_height(tree->left);
 	if (tree->right)
-		counter1 = binary_tree_height(tree->right);
-	if (counter >= counter1)
-		return (1 + counter);
-	return (1 + counter1);
+		counter = binary_tree_height(tree->right);
+	return (1 + counter);
 }
