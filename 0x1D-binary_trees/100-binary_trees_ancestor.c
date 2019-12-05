3bf148699a710c3d2773b4dfb281ffd22c4f69f0
Yesidh <944@holbertonschool.com>
2019-12-05 14:48:59 -0500

    [Fix]: adding more datails in the first if condition

--- 0x1D-binary_trees/100-binary_trees_ancestor.c
+++ 0x1D-binary_trees/100-binary_trees_ancestor.c
@@ -38,7 +38,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	depth_first = binary_tree_depth(first);
 	depth_second = binary_tree_depth(second);
 
-	if (!first || !second)
+	if (!first || !second || (!first->parent && !second->parent))
 		return (NULL);
 
 	if (first->n == second->n)
