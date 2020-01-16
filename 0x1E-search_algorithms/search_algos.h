1c0a38648489ce19c34fe9b48825f0fa916ec6a6
Yesidh <944@holbertonschool.com>
2020-01-16 09:15:28 -0500

    [Feature]: header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGO
+#define SEARCH_ALGO
+
+
+#include <stdlib.h>
+#include <string.h>
+#include<stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
