c82affdd1eeed7b36895ddd7374f5b7d150eb3d6
Yesidh <944@holbertonschool.com>
2020-01-16 09:47:54 -0500

    [Fix]: adding stdio.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,8 +3,9 @@
 
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-#include<stddef.h>
+#include <stddef.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
